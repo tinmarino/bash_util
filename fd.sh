a90b98405361314696a062dba15093afb3c921ea
Tinmarino <tinmarino@gmail.com>
2022-04-16 12:11:51 -0400

    Fork Join, still fighting to spy on stdout without afetcing it

--- fd.sh
+++ fd.sh
@@ -7,7 +7,9 @@ echo test1 >&3  # write to fd 3
 0<&3 read line; echo "$line"
 read -u 3 line; echo "$line"
 { exec < /dev/stdin; cat; } <&3
-{ exec < /dev/stdin; read line; echo "LINE: $line|"; } <&3
+{ exec < /dev/stdin; cat; } <&70
+eval "{ exec < /dev/stdin; cat; } <&3"
+{ exec < /dev/stdin; read -u 70 line; echo "LINE: $line|"; }
 cat - <&3  # read from fd 3
 <&3 cat -
 exec 3>&-  # close fd 3
@@ -26,7 +28,7 @@ show_fseek(){
 
   read -u 3 line; echo "Line: $line"
   echo test3 >&3  # write to fd 3
-  { exec < /dev/stdin; cat; } <&3
+  { exec < /dev/stdin; buffer; } <&3
 
   exec 3>&-  # close fd 3
 }
--- fork_join.sh
+++ fork_join.sh
@@ -1,4 +1,13 @@
+# We start at 70 because 63 is used for process substitution
+# -- like:  echo <(echo 23)  # outputs: /dev/fd/63
+declare -i i_fd_first=70
+
 fork_join(){
+  forkall "$@"
+  joinall "$@"
+}
+
+forkall(){
   ### Asynchronous execute <arg1:s_cmd> for all <args:array> as parameter
   ### <arg1:s_cmd> can have a $1 that will be replaced by each argument
   ### From: https://stackoverflow.com/a/20018504/2544873
@@ -7,21 +16,21 @@ fork_join(){
 
   # Local
   local -a a_pid=()  # TODO
-  local -A d_out=()
-  # We start at 70 because 63 is used for process substitution
-  # -- like:  echo <(echo 23)  # outputs: /dev/fd/63
-  local -i i_fd=0 i i_fd_last=0 i_fd_first=70
-  local s_arg
-
-
 
   # Spawn a thread per file
   # -- For all command
   # -- Get value -> file -> docstring -> fd
   for i in "${!a_arg[@]}"; do
     local s_cmd_cur=${s_cmd//\$1/${a_arg[$i]}}
+    >&2 printf '%02d: %s\n' "$i" "$s_cmd_cur"
     eval "exec $((i + i_fd_first))< <($s_cmd_cur)"
   done
+
+}
+
+joinall(){
+  local -A d_out=()
+  local -i i=0
 
   # Join thread and append docstring -> dictionarie
   for i in "${!a_arg[@]}"; do
@@ -38,9 +47,20 @@ fork_join(){
   for i in "${!d_out[@]}"; do
     echo "$i: ${d_out[$i]}"
   done
+}
 
-  #print_args "${a_arg[@]}"
-  #print_args "$@"
+spy(){
+  ### Read fd in subshell => do not tuch write fd
+  echo "Tin Shell $BASH_SUBSHELL"
+  (
+  echo "Tin Subshell $BASH_SUBSHELL"
+  local -i i=0
+  for i in "$@"; do
+    eval "(
+      cat &
+    ) <&$((i + i_fd_first))"
+  done
+  )
 }
 
 [[ "${BASH_SOURCE[0]}" == "${0}" ]] && fork_join "$@"
